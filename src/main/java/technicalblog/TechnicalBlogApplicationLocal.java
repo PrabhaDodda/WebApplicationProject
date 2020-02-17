ac5b443203e1132b49c7961b730f7f4c04a08aaa
Prabha Dodda <doddaprabha@gmail.com>
2020-02-17 22:27:55 +0800

    First Milestone

---
+++ src/main/java/technicalblog/TechnicalBlogApplicationLocal.java
@@ -0,0 +1,13 @@
+package technicalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalBlogApplicationLocal {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalBlogApplicationLocal.class, args);
+    }
+}
+
